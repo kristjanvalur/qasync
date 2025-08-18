60ee1eab86263739874389d7810780b5edf58ae8
Kristján Valur Jónsson <sweskman@gmail.com>
2025-08-18 14:21:46 +0000

    enable other

--- tests/test_qthreadexec.py
+++ tests/test_qthreadexec.py
@@ -184,7 +184,7 @@ def test_map_timeout(executor):
 
 def test_map_error(executor):
     """Test that map with an exception will raise, and remaining tasks are cancelled"""
-    #pytest.skip()
+    pytest.skip()
     results = []
 
     def func(x):
@@ -204,7 +204,7 @@ def test_map_error(executor):
 
 @pytest.mark.parametrize("cancel", [True, False])
 def test_map_shutdown(executor, cancel):
-    pytest.skip()
+    #pytest.skip()
     results = []
 
     def func(x):
